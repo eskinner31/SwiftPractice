321a050b385831b30417c5ab5b8d5448c5f60285
Eugene Skinner <eskinfrn@gmail.com>
2016-01-14 00:19:03 -0700

    Fixed breakpoint issue

--- FunFactsApp/FunFacts/FunFacts/FactBook.swift
+++ FunFactsApp/FunFacts/FunFacts/FactBook.swift
@@ -19,7 +19,8 @@ struct FactBook {
         "The state of Florida is bigger than England.",
         "Some penguins can leap 2-3 meters out of the water.",
         "On average, it takes 66 days to form a new habit.",
-        "Mammoths still walked the earth when the Great Pyramid was being built." ]
+        "Mammoths still walked the earth when the Great Pyramid was being built."
+    ]
     
     func randomFact() -> String {
         let unsignedArrayCount = UInt32(factsArray.count)
